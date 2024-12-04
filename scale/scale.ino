a82303e9278d2262ef60cb50964900db2f13bb0b
Miguel <mig.ruiz@gmail.com>
2024-12-04 09:50:15 +0000

    ep

--- scale/scale.ino
+++ scale/scale.ino
@@ -1,112 +1,144 @@
-/*
-   -------------------------------------------------------------------------------------
-   HX711_ADC
-   Arduino library for HX711 24-Bit Analog-to-Digital Converter for Weight Scales
-   Olav Kallhovd sept2017
-   -------------------------------------------------------------------------------------
-*/
+#if defined(ESP32)
+#include <analogWrite.h>
+#endif
 
-/*
-   Settling time (number of samples) and data filtering can be adjusted in the config.h file
-   For calibration and storing the calibration value in eeprom, see example file "Calibration.ino"
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#else
+#include <WiFi.h>
+#endif
+#include <PubSubClient.h>
 
-   The update() function checks for new data and starts the next conversion. In order to acheive maximum effective
-   sample rate, update() should be called at least as often as the HX711 sample rate; >10Hz@10SPS, >80Hz@80SPS.
-   If you have other time consuming code running (i.e. a graphical LCD), consider calling update() from an interrupt routine,
-   see example file "Read_1x_load_cell_interrupt_driven.ino".
+const char *ssid = "PIAP";
+const char *password = "hackol37";
+const char *mqttServer = "192.168.0.11";
+const int mqttPort = 1883;
+const char *mqttUser = "";
+const char *mqttPassword = "";
 
-   This is an example sketch on how to use this library
-*/
+WiFiClient wifiClient;
+PubSubClient client(wifiClient); // lib required for mqtt
+
+int connectionTries = 0;
 
 #include <HX711_ADC.h>
 
-#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#if defined(ESP8266) || defined(ESP32) || defined(AVR)
 #include <EEPROM.h>
 #endif
 
-//pins:
-const int HX711_dout = 4; //mcu > HX711 dout pin
-const int HX711_sck = 5; //mcu > HX711 sck pin
+// pins:
+const int HX711_dout = 4; // mcu > HX711 dout pin
+const int HX711_sck = 5;  // mcu > HX711 sck pin
 
-//HX711 constructor:
+// HX711 constructor:
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
 
 const int calVal_eepromAdress = 0;
 const int tareOffsetVal_eepromAdress = 4;
 unsigned long t = 0;
 
-void setup() {
-  Serial.begin(57600); delay(10);
+void WIFI_Connect()
+{
+  WiFi.disconnect();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    connectionTries = connectionTries + 1;
+    if (connectionTries > 20)
+    {
+      connectionTries = 0;
+      return;
+    }
+  }
+
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+  while (!client.connected())
+  {
+    if (client.connect("WINTERCATSCALE", mqttUser, mqttPassword))
+    {
+    }
+    else
+    {
+      delay(2000);
+    }
+  }
+  client.publish("WINTERCAT", "Hello from WINTERCATSCALE");
+}
+
+bool checkWifi()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    WIFI_Connect();
+    return false;
+  }
+  if (!client.connected())
+  {
+    WIFI_Connect();
+    return false;
+  }
+  return true;
+}
+
+void setup()
+{
+  Serial.begin(57600);
+  delay(10);
+  WIFI_Connect();
   Serial.println();
   Serial.println("Starting...");
 
   LoadCell.begin();
-  //LoadCell.setReverseOutput();
-  float calibrationValue; // calibration value (see example file "Calibration.ino")
-  calibrationValue = 18.05; // uncomment this if you want to set the calibration value in the sketch
+  float calibrationValue = 18.05;
 
-#if defined(ESP8266)|| defined(ESP32)
-  EEPROM.begin(512);
-#endif
-
-  //EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
-
-  //restore the zero offset value from eeprom:
-  long tare_offset = 8888954;
-  //EEPROM.get(tareOffsetVal_eepromAdress, tare_offset);
-  LoadCell.setTareOffset(tare_offset);
-  boolean _tare = false; //set this to false as the value has been resored from eeprom
+  LoadCell.setTareOffset(8888954);
+  boolean _tare = false; // set this to false as the value has been resored from eeprom
 
   unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
   LoadCell.start(stabilizingtime, _tare);
-  if (LoadCell.getTareTimeoutFlag()) {
+  if (LoadCell.getTareTimeoutFlag())
+  {
     Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
-    while (1);
+    while (1)
+      ;
   }
-  else {
+  else
+  {
     LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
     Serial.println("Startup is complete");
   }
 }
 
-void loop() {
+void loop()
+{
   static boolean newDataReady = 0;
-  const int serialPrintInterval = 3000; //increase value to slow down serial print activity
+  const int serialPrintInterval = 3000; // increase value to slow down serial print activity
 
   // check for new data/start next conversion:
-  if (LoadCell.update()) newDataReady = true;
+  if (LoadCell.update())
+    newDataReady = true;
 
   // get smoothed value from the dataset:
-  if (newDataReady) {
-    if (millis() > t + serialPrintInterval) {
+  if (newDataReady)
+  {
+    if (millis() > t + serialPrintInterval)
+    {
       float i = LoadCell.getData();
       Serial.print("Load_cell output val: ");
       Serial.println(i);
+
+      if (!checkWifi())
+      {
+        return;
+      }
+      client.publish("WINTERCAT/scale/readings", "str.c_str()");
+
       newDataReady = 0;
       t = millis();
     }
   }
-
-  // receive command from serial terminal, send 't' to initiate tare operation:
-  if (Serial.available() > 0) {
-    char inByte = Serial.read();
-    if (inByte == 't') refreshOffsetValueAndSaveToEEprom();
-  }
-}
-
-// zero offset value (tare), calculate and save to EEprom:
-void refreshOffsetValueAndSaveToEEprom() {
-  long _offset = 0;
-  Serial.println("Calculating tare offset value...");
-  LoadCell.tare(); // calculate the new tare / zero offset value (blocking)
-  _offset = LoadCell.getTareOffset(); // get the new tare / zero offset value
-  EEPROM.put(tareOffsetVal_eepromAdress, _offset); // save the new tare / zero offset value to EEprom
-#if defined(ESP8266) || defined(ESP32)
-  EEPROM.commit();
-#endif
-  LoadCell.setTareOffset(_offset); // set value as library parameter (next restart it will be read from EEprom)
-  Serial.print("New tare offset value:");
-  Serial.print(_offset);
-  Serial.print(", saved to EEprom adr:");
-  Serial.println(tareOffsetVal_eepromAdress);
+  client.loop();
 }
