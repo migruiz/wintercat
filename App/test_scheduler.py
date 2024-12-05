bae53af2a06b85a1ba7db995643eb40679f0ed7e
Sonia Blanco <soniacarolina.blanco@gmail.com>
2024-12-05 16:59:09 +0000

    Merging all streams

--- App/test_scheduler.py
+++ App/test_scheduler.py
@@ -1,8 +1,20 @@
 import scheduler
+import scale
 import random
 import reactivex as rx
 from reactivex import operators as ops
 import time
+from paho.mqtt import client as mqtt_client
+
+# MQTT Broker Config
+broker = '192.168.0.11'
+port = 1883
+
+
+# Create MQTT client
+mqtt_client = mqtt_client.Client()
+mqtt_client.connect(broker, port, 60)
+
 
 class InputObservable(rx.Observable):
     def __init__(self):
@@ -18,10 +30,10 @@ class InputObservable(rx.Observable):
             time.sleep(seconds)
 
 
-input_observable = InputObservable().pipe(ops.map(lambda x: {"type":"master", "state":x }))
+input_observable = InputObservable().pipe(ops.map(lambda x: {"type":"master", "value":x }))
 
-on_cron_observable = scheduler.cron_observable("11:31",True).pipe(ops.map(lambda x: {"type":"cron_on", "state":x }))
-off_cron_observable = scheduler.cron_observable("11:33",False).pipe(ops.map(lambda x: {"type":"cron_off", "state":x }))
+on_cron_observable = scheduler.cron_observable("14:40",True).pipe(ops.map(lambda x: {"type":"cron_on", "state":x }))
+off_cron_observable = scheduler.cron_observable("14:45",False).pipe(ops.map(lambda x: {"type":"cron_off", "state":x }))
 
 merged_cron_observable = rx.merge(on_cron_observable,off_cron_observable)
 
@@ -30,11 +42,18 @@ def buildCronInput(acc,curr):
     return {**acc, 'Last': acc["New"], 'New': curr["state"]}
 
 cron_stream = merged_cron_observable.pipe(
-   ops.scan(buildCronInput, {"Last":False, "New":False}))
+   ops.scan(buildCronInput, {"Last":False, "New":False}),
+   ops.map(lambda x : {"type": "cron","value": x["New"]})
+   )
 
-#Next combine cron_stream with input observable(mqtt_observable mock)
+#scale observable
+scale_observable = scale.scale_observable(mqtt_client)
+scale_stream = scale_observable.pipe( ops.map(lambda x : {"type": "scale","value": True if x["value"] == "in" else False}))
+#subscription = scale_stream.subscribe(lambda x: print("Type:{0}".format(x)))
 
-subscription = cron_stream.subscribe(lambda x: print("Last:{0} New:{1}".format(x["Last"],x["New"])))
+final_observable = rx.merge(cron_stream, scale_stream)
+
+subscription = final_observable.subscribe(lambda x: print("Type:{0} Value:{1}".format(x["type"],x["value"])))
 
 
 try:
@@ -42,5 +61,6 @@ try:
     while True:
         time.sleep(10)
 except KeyboardInterrupt:
+    mqtt_client.disconnect()
     subscription.dispose()
     print("Observer stopped")
