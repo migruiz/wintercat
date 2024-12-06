a2f2eda13997174c97b91ee4e85af6dd87334011
Miguel <mig.ruiz@gmail.com>
2024-12-06 18:04:32 +0000

    distinct values

--- App/scale.py
+++ App/scale.py
@@ -7,9 +7,10 @@ import json
 
 def scale_observable(client:mqtt_client.Client):
 
-    SCALE_TOPIC ="WINTERCAT/house/presence"
+    SCALE_TOPIC ="WINTERCAT/house/presence2"
     return mqttTopicObservable.mqtt_observable(client=client, topic=SCALE_TOPIC).pipe(
         ops.map(lambda x: x == "ON"),
+        ops.distinct_until_changed(),
         ops.map(lambda x: {"type": "scale", "value": x})
     )
 
